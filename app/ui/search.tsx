2e6b1c169e8f3a9ebf639ef265ebd78fc605baba
Sandesh Acharya <mysandesh@gmail.com>
2024-04-26 11:24:23 +0545

    Pathname bug fixes

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,9 +3,9 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search() {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathName = usePathname();
+  const pathname = usePathname();
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
